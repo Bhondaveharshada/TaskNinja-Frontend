9396d3ad8f761c211f83480508f467a4f8f9cb42
Harshada Bhondave <harshadabhondave09@gmail.com>
2025-08-07 15:05:28 +0530

    added live backend url

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -2,15 +2,17 @@ import axios from "axios";
 
 const user = localStorage.getItem('userData');
 
+//for local devlopment --> http://localhost:3000
+
 export const registerloginapi = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://taskninja-6nvm.onrender.com",
     headers:{
         "Content-Type":'application/json'
     }
 })
 
 export const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://taskninja-6nvm.onrender.com",
     headers: {
         "Content-Type": 'application/json',
         Authorization: `Bearer ${user ? JSON.parse(user).token : ''}`
